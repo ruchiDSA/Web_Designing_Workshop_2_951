dba57848e636afb492273d2aa40bec51dd59f601
ruchiDSA <ruchisingh4994@gmail.com>
2026-05-15 16:21:56 +0530

    Experiment_5

---
+++ JavaScript/first.js
@@ -0,0 +1,21 @@
+console.log("Ruchi");
+
+// let sum = [1, 2, 3, 4, 6];
+// let total =
+
+let students =[
+    {name:"Ruchi", marks: 100},
+    {name:"Reema", marks: 89}
+];
+
+let marks = students.map((students => {
+    students.marks
+}));
+console.log(students);  
+
+
+let company = {
+    name: "tech corp",
+
+}
+
